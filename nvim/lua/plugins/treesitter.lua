664dd4d45c8804389a5ebb8afc5ef21a479c4681
citlali <citllainoelia@gmail.com>
2023-03-02 10:39:23 +0000

    Update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function ()
     require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "help", "python", "html" },
+    ensure_installed = { "c", "lua", "vim", "help", "python", "html", "bash", "cpp" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
